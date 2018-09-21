cdfd06a321c711c6a7521d89409d8be35285ca97
sarath <sarathgunalan@gmail.com>
2018-09-21 12:12:51 +0530

    today modify 20/09/18

--- sarath.java
+++ sarath.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World"); //hai
+      System.out.println("Hello, World"); //hai h r u
    }
 
